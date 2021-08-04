92db2808c90cfd34222d2e50f3c1e0acc3c65455
unknown <essidmohamedamin@gmail.com>
2021-08-04 20:58:15 +0100

    update routes

--- controllers/journalsController.js
+++ controllers/journalsController.js
@@ -1,56 +1,50 @@
-'use strict';
+"use strict";
 
+const mongoose = require("mongoose"),
+  Journal = mongoose.model("Journal");
 
-const mongoose = require('mongoose'),
-Journal = mongoose.model('Journal');
-
-exports.list_all_journals = function(req, res) {
-    Journal.find({}, function(err, journal) {
-    if (err)
-      res.send(err);
+exports.list_all_journals = function (req, res) {
+  Journal.find({}, function (err, journal) {
+    if (err) res.send(err);
     res.json(journal);
   });
 };
 
-
-
-
-exports.create_a_journal = function(req, res) {
+exports.create_a_journal = function (req, res) {
   var new_journal = new Journal(req.body);
-  new_journal.save(function(err, journal) {
-    if (err)
-      res.send(err);
+  new_journal.save(function (err, journal) {
+    if (err) res.send(err);
     res.json(journal);
   });
 };
 
-
-exports.read_a_journal = function(req, res) {
-    Journal.findById(req.params.taskId, function(err, journal) {
-    if (err)
-      res.send(err);
+exports.read_a_journal = function (req, res) {
+  Journal.findById(req.params.file, function (err, journal) {
+    if (err) res.send(err);
     res.json(journal);
   });
 };
 
-
-exports.update_a_journal = function(req, res) {
-    Journal.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, journal) {
-    if (err)
-      res.send(err);
-    res.json(journal);
-  });
+exports.update_a_journal = function (req, res) {
+  Journal.findOneAndUpdate(
+    { _id: req.params.taskId },
+    req.body,
+    { new: true },
+    function (err, journal) {
+      if (err) res.send(err);
+      res.json(journal);
+    }
+  );
 };
 
-
-exports.delete_a_journal = function(req, res) {
-
-
-    Journal.remove({
-    _id: req.params.taskId
-  }, function(err, journal) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Journal successfully deleted' });
-  });
+exports.delete_a_journal = function (req, res) {
+  Journal.remove(
+    {
+      _id: req.params.taskId,
+    },
+    function (err, journal) {
+      if (err) res.send(err);
+      res.json({ message: "Journal successfully deleted" });
+    }
+  );
 };
--- routes/journalsRoutes.js
+++ routes/journalsRoutes.js
@@ -1,14 +1,15 @@
-'use strict';
-module.exports = function(app) {
-  var journals = require('../controllers/journalsController');
+"use strict";
+module.exports = function (app) {
+  var journals = require("../controllers/journalsController");
 
   // todoList Routes
-  app.route('/journals')
+  app
+    .route("/journals")
     .get(journals.list_all_journals)
     .post(journals.create_a_journal);
 
-
-  app.route('/journals/:journalId')
+  app
+    .route("/journals/:file")
     .get(journals.read_a_journal)
     .put(journals.update_a_journal)
     .delete(journals.delete_a_journal);
